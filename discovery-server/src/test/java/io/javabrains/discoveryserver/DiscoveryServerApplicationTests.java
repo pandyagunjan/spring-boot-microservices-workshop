8164dc1b7e91887b1486ddda25232794c8856a07
Gunjan Pandya <72512290+pandyagunjan@users.noreply.github.com>
2021-04-20 15:52:42 -0400

    Update DiscoveryServerApplicationTests.java

--- discovery-server/src/test/java/io/javabrains/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/io/javabrains/discoveryserver/DiscoveryServerApplicationTests.java
@@ -2,6 +2,8 @@ package io.javabrains.discoveryserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//No test cases added yet
+
 @SpringBootTest
 class DiscoveryServerApplicationTests {
 
